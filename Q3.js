4197655432c1cd25f921b9657a87b3a80f0638e6
Geovanna <115251617+FranciscaGeovanna@users.noreply.github.com>
2024-11-19 21:16:34 -0300

    Update Q3.js

--- Q3.js
+++ Q3.js
@@ -11,19 +11,19 @@ let resultado;
 
 if(op === "1"){
     resultado = num1 + num2;
-    console.log(resultado);
+    console.log(num1 + " + " + num2 + " = " + resultado);
 } 
 else if(op === "2"){
     resultado = num1 - num2;
-    console.log(resultado);
+    console.log(num1 + " - " + num2 + " = " + resultado);
 }
 else if(op === "3"){
     resultado = num1 * num2;
-    console.log(resultado);
+    console.log(num1 + " X " + num2 + " = " + resultado);
 }
 else if(op === "4"){
     resultado = num1 / num2;
-    console.log(resultado);
+    console.log(num1 + " / " + num2 + " = " + resultado);
 } else{
     console.log("Operação Inválida!")
 }
